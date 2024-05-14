c0abbc867173500228f313248dbc7c25ff0d55b4
Mohammad Mhameed <mohmed6535@gmail.com>
2024-05-14 16:20:27 +0300

    Ionic 6 react js & tailwind css template

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust paths as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
